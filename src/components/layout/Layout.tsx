0d494cad7b14ef6a7bb508589412b31bdb2c401b
prabhu chaturvedi <pchaturvedi754@gmail.com>
2025-06-29 16:09:25 +0530

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import Header from './Header';
+import ChatSupport from '../features/ChatSupport';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,14 +9,19 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 overflow-hidden">
       <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex-1 flex flex-col overflow-hidden lg:ml-0">
         <Header />
-        <main className="flex-1 overflow-y-auto p-6">
-          {children}
+        <main className="flex-1 overflow-y-auto p-4 sm:p-6 custom-scrollbar">
+          <div className="container-responsive">
+            {children}
+          </div>
         </main>
       </div>
+      
+      {/* Global Features */}
+      <ChatSupport />
     </div>
   );
 };
